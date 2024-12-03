adb376e2b0a3fd33ff2b7922870468c9ecbe629b
Nicholas Johnson <njohnson@posteo.net>
2024-12-03 00:00:00 +0000

    Remove unused import

--- altspell/convert.py
+++ altspell/convert.py
@@ -16,7 +16,7 @@
     You should have received a copy of the GNU General Public License
     along with this program.  If not, see <https://www.gnu.org/licenses/>.
 '''
-from flask import Blueprint, request
+from flask import Blueprint
 
 
 bp = Blueprint("convert", __name__, url_prefix='/api')
